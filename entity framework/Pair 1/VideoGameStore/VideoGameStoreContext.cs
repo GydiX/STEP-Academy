e143278ea830fd371685e752d86c051aa9ecfdf2
GidXn <153444661+GydiX@users.noreply.github.com>
2025-02-07 16:19:08 +0300

    ef core - p1

---
+++ entity framework/Pair 1/VideoGameStore/VideoGameStoreContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoGameStore
+{
+    public class VideoGameStoreContext : DbContext
+    {
+        public DbSet<Developer> Developers { get; set; }
+        public DbSet<Game> Games { get; set; }
+
+        public VideoGameStoreContext() : base("VideoGameStoreDb") { }
+    }
+}
