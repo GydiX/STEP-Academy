ad7169f6728bc6b240415bf2a5f9ca3319b61b02
GidXn <153444661+GydiX@users.noreply.github.com>
2025-02-07 18:03:12 +0300

    ef core - p2

---
+++ entity framework/Pair 2/FootballChampionship/Models/AppDbContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootballChampionship.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Team> Teams { get; set; }
+        public DbSet<Player> Players { get; set; }
+        public DbSet<Match> Matches { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost;Database=FootballChampionship;Trusted_Connection=True;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Match>()
+                .HasOne(m => m.Team1)
+                .WithMany()
+                .HasForeignKey(m => m.Team1Id)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<Match>()
+                .HasOne(m => m.Team2)
+                .WithMany()
+                .HasForeignKey(m => m.Team2Id)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
---
+++ entity framework/Pair 2/FootballChampionship/Models/Match.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootballChampionship.Models
+{
+    public class Match
+    {
+        public int Id { get; set; }
+        public int Team1Id { get; set; }
+        public Team Team1 { get; set; }
+        public int Team2Id { get; set; }
+        public Team Team2 { get; set; }
+        public int GoalsTeam1 { get; set; }
+        public int GoalsTeam2 { get; set; }
+        public string Scorer { get; set; }
+        public DateTime MatchDate { get; set; }
+    }
+}
