3a2d7017341068cda2b9f5c6f40d59cc9da57791
GidXn <153444661+GydiX@users.noreply.github.com>
2025-05-02 22:28:40 +0300

    .NET - EF Core - React - Pair 3

---
+++ NET/EF Core - React/Pair 3/users_wf/users_wf/MapperProfiles/UserMapperProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using users_wf.DTOs;
+using users_wf.Models;
+
+namespace users_wf.MapperProfiles
+{
+    public class UserMapperProfile : Profile
+    {
+        public UserMapperProfile()
+        {
+            // UserDTO -> User
+            CreateMap<UserDTO, User>()
+                .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.UserName.ToLower()))
+                .ForMember(dest => dest.Email, opt => opt.MapFrom(src => src.Email.ToLower()))
+                .ForMember(dest => dest.Id, opt => opt.MapFrom(src => Guid.NewGuid().ToString()))
+                .ForMember(dest => dest.RoleId, opt => opt.MapFrom(src => src.Role))
+                .ForMember(dest => dest.Role, opt => opt.Ignore());
+        }
+    }
+}
