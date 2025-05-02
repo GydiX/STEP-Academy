121b5fe535b9c4f8fa4c959940d525a75e3e97cb
GidXn <153444661+GydiX@users.noreply.github.com>
2025-05-02 22:11:16 +0300

    .NET - EF Core - React - Pair 1

---
+++ NET/EF Core - React/Pair 1/VideoGameStore/Game.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VideoGameStore
+{
+    public class Game
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public decimal Price { get; set; }
+        public bool IsMultiplayer { get; set; }
+        public int DeveloperId { get; set; }
+        public virtual Developer Developer { get; set; }
+    }
+}
---
+++ NET/EF Core - React/Pair 1/VideoGameStore/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace VideoGameStore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VideoGameStoreService service = new VideoGameStoreService();
+
+            // Тестування методів для "Розробники"
+            var developer = new Developer { Name = "Epic Games", EstablishedDate = new DateTime(1991, 1, 1) };
+            service.AddDeveloper(developer);
+
+            var developers = service.GetAllDevelopers();
+            Console.WriteLine("Всі розробники:");
+            foreach (var dev in developers)
+            {
+                Console.WriteLine($"ID: {dev.Id}, Назва: {dev.Name}, Дата заснування: {dev.EstablishedDate}");
+            }
+
+            // Оновлення розробника
+            developer.Name = "Epic Games Updated";
+            service.UpdateDeveloper(developer);
+
+            // Отримання розробника по назві
+            var foundDev = service.GetDeveloperByName("Epic Games Updated");
+            Console.WriteLine($"Знайдений розробник: {foundDev.Name}");
+
+            // Видалення розробника
+            service.RemoveDeveloper(foundDev.Id);
+
+            // Тестування методів для "Ігри"
+            var game = new Game { Title = "Fortnite", Price = 0, IsMultiplayer = true, DeveloperId = developer.Id };
+            service.AddGame(game);
+
+            var gamesInRange = service.GetGamesByPriceRange(0, 60);
+            Console.WriteLine("Ігри у вказаному ціновому діапазоні:");
+            foreach (var g in gamesInRange)
+            {
+                Console.WriteLine($"Назва: {g.Title}, Ціна: {g.Price}");
+            }
+
+            var multiplayerGames = service.GetMultiplayerGames();
+            Console.WriteLine("Багатокористувацькі ігри:");
+            foreach (var g in multiplayerGames)
+            {
+                Console.WriteLine($"Назва: {g.Title}");
+            }
+        }
+    }
+}
